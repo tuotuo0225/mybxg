24e41f1022316cf661d4f9ae8c6f030a1bff2b75
tuotuo0225 <513519557@qq.com>
2017-03-19 19:13:47 +0800

    完善登录

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 	$filename='index';
 	if(array_key_exists('PATH_INFO', $_SERVER)){
 		//var_dump($_SERVER);
+		// /index/index===index
 		$url=$_SERVER['PATH_INFO'];
 		$str=substr($url,1);
 		$arr=explode('/',$str);
 		if(count($arr)==2){
 			$path=$arr[0];
 			$filename=$arr[1];
+		}else{
+		    $filename='login';
 		}
 		//echo $url;
 		//载入指定路径页面
--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,10 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	var pathname=location.pathname;
+	var flag=$.cookie('PHPSESSID');
+	if(!flag && pathname.indexOf('login')==-1){
+		//没有登录--跳转到登录页
+		location.href='/index/login';
+	}
