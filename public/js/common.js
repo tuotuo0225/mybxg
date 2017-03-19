74a816c9edf70a6d412d4f7cb8f4e3bcd8360662
tuotuo0225 <513519557@qq.com>
2017-03-19 20:09:36 +0800

    公共部分提取

--- public/js/common.js
+++ public/js/common.js
@@ -1,15 +1,12 @@
 
-	NProgress.start();
-
-	NProgress.done();
 
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
 
 	var pathname=location.pathname;
-	var flag=$.cookie('PHPSESSID');
+	var flag= $.cookie('PHPSESSID');
 	if(!flag && pathname.indexOf('login')==-1){
 		//没有登录--跳转到登录页
-		location.href='/index/login';
+		location.href='/login';
 	}
