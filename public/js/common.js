8f1140fcfe1aec192b8e3e999b4626972e96738a
tuotuo0225 <513519557@qq.com>
2017-03-20 16:56:06 +0800

    完善登录功能 添加退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,12 +1,62 @@
 
-
+define(['jquery','cookie'],function($){
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
 
 	var pathname=location.pathname;
-	var flag= $.cookie('PHPSESSID');
+	var flag=$.cookie('PHPSESSID');
 	if(!flag && pathname.indexOf('login')==-1){
 		//没有登录--跳转到登录页
 		location.href='/login';
 	}
+	//登录功能
+	$("#loginForm").submit(function(){
+		//alert(123);
+		var formData=$(this).serialize();
+		console.log(formData);
+		$.ajax({
+			type:'post',
+			url:'/api/login',
+			data:formData,
+			dataType:'json',
+			success:function(data){
+				console.log(data);
+				if(data.code==200){
+					console.log(data.result);
+					//cookie 存的是字符串---将对象转换成字符串
+					var logInfo=JSON.stringify(data.result);
+					//设置cookie--设置到根目录
+					$.cookie('logInfo',logInfo,{path:'/'});
+					location.href='/index/index';
+				}
+			},
+			error:function(data){
+				console.log(data);
+			}
+		});
+		return false; //阻止表单提交默认行为
+	});
+
+	//退出功能
+	$("#logout").click(function(){
+		$.ajax({
+			type:"post",
+			url:"/api/logout",
+			dataType:"json",
+			success:function(data){
+				if(data.code==200){
+					location.href="/login";
+				}
+			}
+		});
+	});
+	//console.log($.cookie('logInfo'));
+	var obj=JSON.parse($.cookie('logInfo'));
+	//console.log(obj.tc_name);
+	//console.log(obj.tc_avatar);
+	$(".aside .profile img").attr("src",obj.tc_avatar);
+	$(".aside .profile h4").html(obj.tc_name);
+
+});
+	
---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hasee on 2017/3/19.
+ */
+require.config({
+    baseUrl:'/public',
+    paths:{
+        jquery:'assets/jquery/jquery.min',
+        cookie:'assets/jquery-cookie/jquery.cookie',
+        echarts:'assets/echarts/echarts.min'
+    }
+});
